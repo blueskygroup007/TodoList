70194eb0924256fd5d22cec8acd8dea41c3ac7cf
bluesskymac <blueskymac@123.com>
2023-02-07 15:41:39 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by 李海永 on 2023/2/7.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
